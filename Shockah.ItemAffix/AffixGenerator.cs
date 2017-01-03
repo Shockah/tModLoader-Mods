f5aceda5a50d4460420a1ec4dc342f2ee0db79ac
Michael Dolas <lolshockah@gmail.com>
2017-01-04 00:01:53 +0100

    [ItemAffix] Initial generator code

---
+++ Shockah.ItemAffix/AffixGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Shockah.Affix.Utils;
+using System;
+using System.Collections.Generic;
+using Terraria;
+
+namespace Shockah.Affix.Content
+{
+	public class AffixGenerator
+	{
+		public readonly List<AffixGeneratorEntry> entries = new List<AffixGeneratorEntry>();
+
+		public void Generate(Item item, AffixGeneratorSource source)
+		{
+
+		}
+	}
+
+	public class AffixGeneratorSource
+	{
+		protected readonly Dynamic<float> value;
+
+		public AffixGeneratorSource(float value)
+		{
+			this.value = value;
+		}
+
+		public virtual float GetValue(Item item)
+		{
+			return value;
+		}
+	}
+
+	public class AffixGeneratorEntry
+	{
+		public readonly float weight;
+		public readonly Func<Affix> @delegate;
+
+		public AffixGeneratorEntry(float weight, Func<Affix> @delegate)
+		{
+			this.weight = weight;
+			this.@delegate = @delegate;
+		}
+	}
+}
